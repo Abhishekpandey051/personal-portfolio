671eef5688566e851f00d91be24da1d6f3ca0e24
Abhishek Pandey <abhi.ecc.cs@gmail.com>
2025-12-29 16:52:56 +0530

    bug fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         data-aos-easing='ease-in-out'
       >
         <span className='text-lg font-bold text-orange-700'>
-          {/* FULL-STACK DEVELOPER */}
+          FULL-STACK DEVELOPER
         </span>
         <div className='flex gap-4 items-center my-4'>
           <h1 className='text-gray-800 dark:text-neutral-100  md:text-5xl 2xl:text-7xl font-bold text-center'>
@@ -28,8 +28,8 @@ const Header = () => {
         <TypeAnimation
       sequence={[
         "I'm Abhishek Pandey", 2000,
-        "I'm Software Developer" ,
-        2000,
+        // "I'm Software Developer" ,
+        // 2000,
         "I'm Front-End Developer" ,
         2000,
       ]}
@@ -64,7 +64,7 @@ const Header = () => {
       <div className='w-full flex-1 flex justify-center'>
         <div className='w-[300px] h-[290px] md:w-[400px] md:h-[400px] 2xl:w-[500px] 2xl:h-[500px]  border rounded-full border-gray-600 dark:border-gray-200 relative  md:mt-20 lg:mt-0 flex items-center justify-center'>
           <img
-            src={Reactjs}
+            src={Profile}
             alt="img"
             className='w-auto h-full absolute -top-5 md:-top-10'
             data-aos='fade-down'
